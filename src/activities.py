f30716900577f6c75578811bc5696ff6e50078e2
Nico Marais <84723800+Liedjieboer@users.noreply.github.com>
2024-11-06 16:19:55 +0000

    Started developing an Activities class in VSCode and tested data collection in activities notebook

--- src/activities.py
+++ src/activities.py
@@ -1,13 +1,25 @@
 import pandas as pd
 import numpy as np
 import datetime
-from src.data import Bellabeat
+from data import Bellabeat
 
 class Activities:
     '''
     DataFrames containing user id's as index,
-    and tracking data as columns
+    and tracking data as columns for the activities table
     '''
     def __init__(self):
-        # Assign an attribute ".data" to all new instances of Order
+        # Assign an attribute ".data" to all new instances of Activities
         self.data = Bellabeat().get_data()
+
+    def get_activities_data(self):
+        """
+        Returns a clean DataFrame (without NaN), with the following columns:
+        ['order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected',
+        'order_status', 'dim_is_five_star', 'dim_is_one_star', 'review_score',
+        'number_of_products', 'number_of_sellers', 'price', 'freight_value',
+        'distance_seller_customer']
+        """
+
+        df = Bellabeat().get_data()['Activity']
+        return df
